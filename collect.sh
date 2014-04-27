7e57b9272e46b8f52db6719cc1c877355d819888
jcchen <junchangchen@gmail.com>
2014-04-28 04:50:24 +0800

    init repo 2.

---
+++ collect.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+user=`whoami`
+
+cp -f /home/$user/.xmonad/session session
+cp -f /home/$user/.xmonad/xmonad.hs xmonad.hs
+cp -f /usr/share/xsessions/xmonad.desktop xmonad.desktop
+cp -f /usr/bin/xmonad-start xmonad-start
+cp -f /home/$user/.xmobarrc _xmobarrc
+
--- xmonad.hs
+++ xmonad.hs
@@ -1,1 +1,31 @@
-/home/jcchen/.xmonad/xmonad.hs
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/home/jcchen/.cabal/bin/xmobar /home/jcchen/.xmobarrc"
+    xmonad $ defaultConfig
+      { manageHook = manageDocks <+> myManageHook 
+                     <+> manageHook defaultConfig
+      , layoutHook = avoidStruts  $  layoutHook defaultConfig
+      , logHook = dynamicLogWithPP xmobarPP
+                  { ppOutput = hPutStrLn xmproc
+                  , ppTitle = xmobarColor "green" "" . shorten 50
+                  }
+      , modMask = mod4Mask
+      , terminal = "xfce4-terminal"
+      , borderWidth = 3
+      } `additionalKeys`
+      [ ((controlMask .|. shiftMask, xK_Delete), spawn "xscreensaver-command -lock")
+      , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+      , ((0, xK_Print), spawn "scrot")
+      ]
+
+
+myManageHook = composeAll
+               [ className =? "Gimp" --> doFloat
+               , className =? "Vncviewer" --> doFloat
+               ]
